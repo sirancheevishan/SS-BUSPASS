301586fc5a452d74b2552bc65841a49fa97b98e9
sirancheevisan <sirancheevishan@gmail.com>
2021-04-26 23:26:08 +0530

    Update booking track.

--- BusPassApp/Controllers/PaymentController.cs
+++ BusPassApp/Controllers/PaymentController.cs
@@ -1,6 +1,9 @@
-﻿using Newtonsoft.Json;
+﻿using BusPassApp.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -11,7 +14,7 @@ namespace BusPassApp.Controllers
     public class PaymentController : Controller
     {
         // GET: Payment
-           
+        BusPassApp.WSDL_Service.BusPassApp Ws_Service = new WSDL_Service.BusPassApp();
         public ActionResult Index(Models.PaymentInitiateModel _requestData)
         {
             Session.Add("PaymentString", JsonConvert.SerializeObject(_requestData));
@@ -91,33 +94,88 @@ namespace BusPassApp.Controllers
         [HttpPost]
         public ActionResult Complete()
         {
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
-            
-            string paymentId = Request.Params["rzp_paymentid"];
+            string strUserID = Convert.ToString(Session["REGISTERED_NO"]);
+            string strPhnNo = Convert.ToString(Session["MOBILE_NO"]);
+            string strMailID = Convert.ToString(Session["MAIL_ID"]);
+            string strBookingReq = string.Empty;
+            string strXMLData = string.Empty;
+            string strtime = string.Empty;
+            string strErrMSG = string.Empty;
+            DataSet ds_result = new DataSet();
 
-            // This is orderId
-            string orderId = Request.Params["rzp_orderid"];
-
-            Razorpay.Api.RazorpayClient client = new Razorpay.Api.RazorpayClient("rzp_test_PUIxVIZT1Ae6fR", "lvOoPdVuu9fmkaBG8bnF7qdy");
-
-            Razorpay.Api.Payment payment = client.Payment.Fetch(paymentId);
-
-            // This code is for capture the payment 
-            Dictionary<string, object> options = new Dictionary<string, object>();
-            options.Add("amount", payment.Attributes["amount"]);
-            Razorpay.Api.Payment paymentCaptured = payment.Capture(options);
-            string amt = paymentCaptured.Attributes["amount"];
-
-            //// Check payment made successfully
-
-            if (paymentCaptured.Attributes["status"] == "captured")
+            try
             {
-                // Create these action method
-                return RedirectToAction("Success");
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+
+                string paymentId = Request.Params["rzp_paymentid"];
+                string orderId = Request.Params["rzp_orderid"];
+
+                Razorpay.Api.RazorpayClient client = new Razorpay.Api.RazorpayClient("rzp_test_PUIxVIZT1Ae6fR", "lvOoPdVuu9fmkaBG8bnF7qdy");
+
+                Razorpay.Api.Payment payment = client.Payment.Fetch(paymentId);
+
+                Dictionary<string, object> options = new Dictionary<string, object>();
+                options.Add("amount", payment.Attributes["amount"]);
+                Razorpay.Api.Payment paymentCaptured = payment.Capture(options);
+                string amt = paymentCaptured.Attributes["amount"];
+
+                
+                if (paymentCaptured.Attributes["status"] == "captured")
+                {
+
+                    #region Update Track
+                    BaseClass.BookingRQRS BookingRQRS = new BaseClass.BookingRQRS();
+                    string strCurrency = ConfigurationManager.AppSettings["Currency"] != null && ConfigurationManager.AppSettings["Currency"].ToString() != "" ? ConfigurationManager.AppSettings["Currency"].ToString() : "";
+                    BookingRQRS.strPassType = "";
+                    BookingRQRS.strUserID = strUserID;
+                    BookingRQRS.stTrackID = "";
+                    BookingRQRS.strPassNo = "";
+                    BookingRQRS.strRemark = "";
+                    BookingRQRS.strBookedDate = "";
+                    BookingRQRS.strUpdateDate = "";
+                    BookingRQRS.strRemark = "";
+                    BookingRQRS.strFlag = "U";
+                    BookingRQRS.strStatus = "S";
+                    BookingRQRS.strMail = strMailID;
+                    BookingRQRS.strPhnNo = strPhnNo;
+                    BookingRQRS.strCurrency = strCurrency;
+                    strBookingReq = JsonConvert.SerializeObject(BookingRQRS);
+
+                    Session.Add("BooingDetails", JsonConvert.SerializeObject(BookingRQRS));
+                    //RequestLog
+                    strtime = DateTime.Now.ToString("DDMMYYYY HH:MM:SS:FFFF");
+                    strXMLData = "<EVENT><REQUEST>inserRegistrationDetails</REQUEST>";
+                    strXMLData += "<REQUESTTIME>" + strtime + "</REQUESTTIME><EVENT>";
+                    strXMLData += "<REQUESTDATA>" + strBookingReq + "</REQUESTDATA><EVENT>";
+
+                    ds_result = Ws_Service.InertFetchBookingDetails(strBookingReq, ref strErrMSG);
+
+                    //ResponseLog
+                    strtime = DateTime.Now.ToString("DDMMYYYY HH:MM:SS:FFFF");
+                    strXMLData = "<EVENT><RESPONSE>inserRegistrationDetails</RESPONSE>";
+                    strXMLData += "<RESTTIME>" + strtime + "</RESTTIME>";
+                    strXMLData += "<ERROMSG>" + strErrMSG + "</ERROMSG>";
+                    strXMLData += "<RESDATA>" + JsonConvert.SerializeObject(ds_result) + "</RESDATA><EVENT>";
+
+                    if (ds_result != null && ds_result.Tables.Count > 0 && ds_result.Tables[0].Rows.Count > 0)
+                    {
+                        if (ds_result.Tables[0].Rows[0]["Result"].ToString() == "1")
+                        {
+
+                        }
+                    }
+                    #endregion
+
+                    return RedirectToAction("Success");
+                }
+                else
+                {
+                    return RedirectToAction("Failed");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                return RedirectToAction("Failed");
+                return RedirectToAction("Success");
             }
         }
 
